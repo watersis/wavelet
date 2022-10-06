8c7cb696f4ed2d3c039cf2007104c1af61be4492
watersis <114371045+watersis@users.noreply.github.com>
2022-10-06 11:50:07 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -4,43 +4,32 @@ import java.util.ArrayList;
 class Handler1 implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+
     ArrayList<String> s1 = new ArrayList<String>();
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } 
-        else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } 
-        else{
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return "";
-                }
+        if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
                     s1.add(parameters[1]);
-                    return "";
+                    return parameters[1];
+                }
+        }
+        else if (url.getPath().contains("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            for(int i=0;i<s1.size();i+=1){
+                if(s1.get(i).contains(parameters[1])){
+                    result.add(s1.get(i));
                 }
             }
-            if (url.getPath().contains("/search")) {
-                String[] parameters = url.getQuery().split("=");
-    
-                for(int i=0;i<s1.size();i+=1){
-                    if(s1.get(i).contains(parameters[1])){
-                        return String.format(s1.get(i));
-                    }
-                }
-            }
-                          
+            return String.format(s1);
+        }
+        else{
             return "404 Not Found!";
+        }                 
+        
             
 
-        }
+        
     }
 }
 
